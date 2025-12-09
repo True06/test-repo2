50cb5393045991b2581ee44b414aae7753e1aa71
True06 <ahnyejun01@gmail.com>
2025-12-09 16:21:54 +0900

    Update main.py (Subtract ver.)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Ender the second number"))
 
-result = num1 + num2
+result = num1 - num2
 
-print("Sum: ", result)
+print("Subtract: ", result)
