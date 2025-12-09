debd358c6fa20e0ae67a6d230c7def1f4c90aba2
True06 <ahnyejun01@gmail.com>
2025-12-09 16:34:16 +0900

    Final modification

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Ender the second number"))
 
-result = num1 * num2
+mul_result = num1 * num2
+sum_result = num1 + num2
+sub_result = num1 - num2
 
-print("Multiply: ", result)
+print("Sum: ", sum_result)
+print("Subtract: ", sub_result)
+print("Multiply: ", mul_result)
